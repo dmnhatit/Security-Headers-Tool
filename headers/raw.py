2883d208a0c0021af247a686691bd468ce10302d
dmnhatit <21110842@student.hcmute.edu.vn>
2023-12-24 23:56:36 +0700

    update print

--- headers/raw.py
+++ headers/raw.py
@@ -2,4 +2,4 @@ def rawHeaders(url, headers):
     #raw headers
     print("Raw Headers:")
     for key, value in headers.items():
-        print(key, " : ", value)
+        print("+", key, ":", value)
